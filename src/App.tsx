76d564a1ff74c6685272d1e21d4bcc279625ab1a
MBTJcodes <103816352+MBTJcodes@users.noreply.github.com>
2024-11-09 12:18:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,18 @@ import Layout from './components/Layout';
 import { WalletProvider } from './context/WalletContext';
 
 function App() {
-  // Check if we're on the landing page
-  const isLandingPage = window.location.pathname === '/';
-
   return (
     <WalletProvider>
-      {isLandingPage ? (
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      ) : (
-        <Layout>
-          <Routes>
-            <Route path="/app" element={<Dashboard />} />
-            <Route path="/app/create" element={<CreateCoin />} />
-            <Route path="/app/coin/:id" element={<CoinDetails />} />
-          </Routes>
-        </Layout>
-      )}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route element={<Layout />}>
+          <Route path="/app" element={<Dashboard />} />
+          <Route path="/app/create" element={<CreateCoin />} />
+          <Route path="/app/coin/:id" element={<CoinDetails />} />
+        </Route>
+      </Routes>
     </WalletProvider>
   );
 }
+
+export default App;
